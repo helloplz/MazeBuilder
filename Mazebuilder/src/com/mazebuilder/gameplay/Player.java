f279756de6a06acfc9cdbb0df9546e14328e03db
pgruevski <predrag_gruevski@yahoo.com>
2012-09-23 01:49:00 -0400

    Fixed ambiguous description in javadoc.

--- Mazebuilder/src/com/mazebuilder/gameplay/Player.java
+++ Mazebuilder/src/com/mazebuilder/gameplay/Player.java
@@ -4,21 +4,21 @@ import com.google.common.collect.Multiset;
 import com.mazebuilder.renderer.Renderable;
 
 public interface Player extends Renderable {
-	
-	String getName();
-	
-	boolean canJump();
-	
-	/** Should be called every turn -- will handle matters such as drawing movement bonuses etc. **/
-	boolean executeTurn();
-	
+
+    String getName();
+
+    boolean canJump();
+
+    /** Should be called every turn -- will handle matters such as generating movement bonuses etc. **/
+    boolean executeTurn();
+
     /** Returns the set of bonuses for the player. **/
     Multiset<Direction> getBonuses();
-    
+
     /** Returns the number of bonuses the player needs to spend to jump a wall **/
     int bonusesToJump();
-    
+
     /** Removes a bonus from the player. **/
     boolean spendBonus(Direction d);
-	
+
 }

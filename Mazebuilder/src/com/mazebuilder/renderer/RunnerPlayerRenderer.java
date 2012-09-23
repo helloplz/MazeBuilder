262a9e4b3dcbbb8d3b3ca94fce9f9384a8de0bee
pgruevski <predrag_gruevski@yahoo.com>
2012-09-23 18:18:43 -0400

    Updated renderers with sample player sprites.

--- Mazebuilder/src/com/mazebuilder/renderer/RunnerPlayerRenderer.java
+++ Mazebuilder/src/com/mazebuilder/renderer/RunnerPlayerRenderer.java
@@ -2,12 +2,25 @@ package com.mazebuilder.renderer;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 
 public final class RunnerPlayerRenderer implements PlayerRenderer {
+
+    private final Image player;
+
+    public RunnerPlayerRenderer() {
+        try {
+            player = new Image("\\assets\\Character Cat Girl.png");
+        } catch (SlickException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public void drawPlayer(Graphics g, int x, int y, int tileWidth, int tileHeight) {
         g.setColor(Color.green);
-        g.fillRoundRect(x, y, tileWidth, tileHeight, Math.min(tileHeight, tileWidth) / 2);
+        g.drawImage(player, x, y, x + tileWidth, y + tileHeight, 0, 50, 101, 151);
+        // g.fillRoundRect(x, y, tileWidth, tileHeight, Math.min(tileHeight, tileWidth) / 2);
     }
 }

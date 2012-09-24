4dd2a9d3c2cab9ab8f5809bc48c7b706dd6d7bc1
Lasagna220 <Lasagna220@18.111.89.118>
2012-09-24 11:30:10 -0400

    Added basic testing strategy.

---
+++ Mazebuilder/test/com/mazebuilder/toys/TestGame.java
@@ -0,0 +1,23 @@
+package test.com.mazebuilder.toys;
+
+public class TestGame {
+    /**
+     * TESTING STRATEGY:
+     * 
+     * 1. Test basic features of player input individually 
+     *      (i.e. Player A move, player B move, player A place wall, 
+     *      player B special move)
+     * 2. Test boundary conditions associated with player input
+     *      (players can't leave board or move through walls, etc)
+     * 3. Test win conditions for both players
+     * 4. Test full game scenarios for expected progression 
+     *      of game states. 
+     * 
+     * Tests will be done using a combination of error messages to 
+     * check for illegal moves, and board state logging to check 
+     * for the correct progression of game states.     
+     *  
+     * Player input for tests will be simulated through hard-coding 
+     * sequences of inputs.
+     */
+}

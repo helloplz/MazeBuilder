f9d888b3ca8e9ee3b22b31e7fbf5beda4e595dde
Lasagna220 <Lasagna220@18.111.122.54>
2012-09-26 16:59:57 -0400

    Added Win States

---
+++ Mazebuilder/src/com/mazebuilder/root/RunnerWinState.java
@@ -0,0 +1,42 @@
+package com.mazebuilder.root;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class RunnerWinState extends BasicGameState {
+
+    static public final int ID = 3;
+    private StateBasedGame game;
+    private GameContainer gameContainer;
+
+    @Override
+    public void init(GameContainer container, StateBasedGame game) throws SlickException {
+        // TODO Auto-generated method stub
+        this.game = game;
+        this.gameContainer = container;
+
+    }
+
+    @Override
+    public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+        g.drawString("Running Player Wins!", 50, 100);
+    }
+
+    @Override
+    public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException {
+    }
+
+    @Override
+    public void mouseClicked(int button, int x, int y, int clickCount) {
+        game.enterState(GameplayState.ID);
+    }
+
+    @Override
+    public int getID() {
+        return ID;
+    }
+
+}

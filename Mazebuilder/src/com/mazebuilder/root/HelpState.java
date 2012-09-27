8b164ead75e452f1d1bc946915a5f374a6c72e86
Lasagna220 <Lasagna220@18.111.122.54>
2012-09-27 02:10:31 -0400

    Added Help Screen.png to Assets. This will be the name of the image file used on the help page. Needs to be replaced with something legible at some point.

--- Mazebuilder/src/com/mazebuilder/root/HelpState.java
+++ Mazebuilder/src/com/mazebuilder/root/HelpState.java
@@ -2,6 +2,7 @@ package com.mazebuilder.root;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.BasicGameState;
@@ -14,6 +15,8 @@ public class HelpState extends BasicGameState {
 
     private Rectangle MenuButton;
 
+    private Image HelpImg;
+
     @Override
     public void init(GameContainer container, StateBasedGame game) throws SlickException {
         this.game = game;
@@ -22,8 +25,14 @@ public class HelpState extends BasicGameState {
     @Override
     public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
         g.drawString("HINT: the cake is a lie...", 50, 100);
-        g.draw(MenuButton = new Rectangle(48, 148, 100, 48));
-        g.drawString("Main Menu", 50, 150);
+        try {
+            HelpImg = new Image("./assets/Help Screen.png");
+            g.drawImage(HelpImg, 10, 150);
+        } catch (SlickException e) {
+            throw new RuntimeException(e);
+        }
+        g.draw(MenuButton = new Rectangle(48, 678, 100, 48));
+        g.drawString("Main Menu", 50, 680);
     }
 
     @Override

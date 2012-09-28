24d50a74f096f533508130ec259dd0d66d56187e
dxiao <dxiao@mit.edu>
2012-09-27 23:53:57 -0400

    Added executable!

--- Mazebuilder/src/com/mazebuilder/root/MazebuilderGame.java
+++ Mazebuilder/src/com/mazebuilder/root/MazebuilderGame.java
@@ -16,7 +16,7 @@ public class MazebuilderGame extends StateBasedGame {
     private Player winner;
 
     public MazebuilderGame() {
-        super("Mazebuilder v0.2");
+        super("Crate Escape v0.2");
     }
 
     @Override
